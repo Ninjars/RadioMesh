e5355091ae682d3e76f685332e30e0a1da9a8c06
jez <jeremy+github@jeremystevens.net>
2017-10-21 15:08:08 +0100

    rolled back the quad tree force calculations to fix issues with simple node-node forces

--- app/src/main/java/com/ninjarific/radiomesh/ui/clusters/NodeForceCalculator.java
+++ app/src/main/java/com/ninjarific/radiomesh/ui/clusters/NodeForceCalculator.java
@@ -1,7 +1,6 @@
 package com.ninjarific.radiomesh.ui.clusters;
 
 import com.ninjarific.radiomesh.forcedirectedgraph.QuadTree;
-import com.ninjarific.radiomesh.utils.Bounds;
 import com.ninjarific.radiomesh.utils.Coordinate;
 
 import timber.log.Timber;
@@ -27,13 +26,25 @@ public class NodeForceCalculator {
     }
 
     void repelNode(ForceConnectedNode node, QuadTree<ForceConnectedNode> quadTree) {
-        QuadTree<ForceConnectedNode> leaf = node.getContainingLeaf();
-        if (leaf != null) {
-            applyTreeForce(leaf, quadTree);
+        applySimpleRecursiveNBodyForces(node, quadTree);
+    }
+
+    private void applySimpleRecursiveNBodyForces(ForceConnectedNode node, QuadTree<ForceConnectedNode> tree) {
+        // repel
+        for (ForceConnectedNode other : tree.getContainedItems()) {
+            if (other != node) {
+                double dx = other.getX() - node.getX();
+                double dy = other.getY() - node.getY();
+                applyRepulsionForce(node, dx, dy, 1);
+            }
+        }
+
+        for (QuadTree<ForceConnectedNode> subtree : tree.getSubTrees()) {
+            applySimpleRecursiveNBodyForces(node, subtree);
         }
     }
 
-    private void applyRepulsionForce(ForceConnectedNode node, double dx, double dy, double multiplier, double scalingFactor) {
+    private void applyRepulsionForce(ForceConnectedNode node, double dx, double dy, double multiplier) {
         double mag = Math.sqrt(dx * dx + dy * dy);
         if (mag == 0) {
             return;
@@ -41,55 +52,43 @@ public class NodeForceCalculator {
         double vx = dx / mag;
         double vy = dy / mag;
 
-        //                double vx = Math.min(1, Math.max(-1, dx / mag));
-        //                double vy = Math.min(1, Math.max(-1, dy / mag));
-        //                dx = dx / worldSize;
-        //                dy = dy / worldSize;
-        double magnitude = -multiplier * scalingFactor;
-        double fx = dx < MINIMUM_NODE_DISTANCE && dx > -MINIMUM_NODE_DISTANCE ? 0 : magnitude / dx;
-        double fy = dy < MINIMUM_NODE_DISTANCE && dy > -MINIMUM_NODE_DISTANCE ? 0 : magnitude / dy;
+        double force = -Math.min(maxForce, multiplier * forceMagnitude / mag);
+        double fx = vx * force;
+        double fy = vy * force;
 
-        //                if (fx > maxForce) {
-        //                    fx = maxForce;
-        //                } else if (fx < -maxForce) {
-        //                    fx = -maxForce;
-        //                }
-        //                if (fy > maxForce) {
-        //                    fy = maxForce;
-        //                } else if (fy < -maxForce) {
-        //                    fy = -maxForce;
-        //                }
-        node.applyForce(vx, vy);
-        Timber.i("applyRepulsionForce " + node.getIndex() + " multiplied by: " + multiplier + "   dx,dy: " + dx + ", " + dy + "   vx,vy: " + vx + ", " + vy + "   fx,fy: " + fx + ", " + fy);
+        node.addForce(fx, fy);
+        Timber.i("applyRepulsionForce " + node.getIndex() + " force: " + force + " multiplied by: " + multiplier + "   dx,dy: " + dx + ", " + dy + "   vx,vy: " + vx + ", " + vy + "   fx,fy: " + fx + ", " + fy);
     }
 
-    private void applyTreeForce(QuadTree<ForceConnectedNode> leaf, QuadTree<ForceConnectedNode> tree) {
+    private void applyTreeForce(ForceConnectedNode node, QuadTree<ForceConnectedNode> tree) {
         if (tree.isEmpty()) return;
-        double distance = quadTreeDistance(leaf, tree);
-        if (leaf == tree) {
-            for (ForceConnectedNode nodeA : leaf.getContainedItems()) {
-                for (ForceConnectedNode nodeB : tree.getContainedItems()) {
-                    if (nodeA != nodeB) {
-                        double dx = nodeB.getX() - nodeA.getX();
-                        double dy = nodeB.getY() - nodeA.getY();
-                        applyRepulsionForce(nodeA, dx, dy, 1, forceMagnitude);
-                    }
-                }
-            }
-
-        } else if (leafIsFar(distance, tree) || tree.isLeaf()) {
-            // apply repulsive force
-            int forceMultiplier = tree.getTotalContainedItemCount();
-            for (ForceConnectedNode node : leaf.getContainedItems()) {
-                double dx = getDx(node, tree);
-                double dy = getDy(node, tree);
-                applyRepulsionForce(node, dx, dy, forceMultiplier, forceMagnitude);
-            }
-        } else {
-            for (QuadTree<ForceConnectedNode> treeNode : tree.getSubTrees()) {
-                applyTreeForce(leaf, treeNode);
+        double distance = quadTreeDistance(node, tree);
+//        if (node.getContainingLeaf() == tree) {
+        for (ForceConnectedNode other : tree.getContainedItems()) {
+            if (other != node) {
+                double dx = other.getX() - node.getX();
+                double dy = other.getY() - node.getY();
+                applyRepulsionForce(node, dx, dy, 1);
             }
         }
+
+        for (QuadTree subtree : tree.getSubTrees()) {
+            applyTreeForce(node, subtree);
+        }
+
+//        } else if (leafIsFar(distance, tree) || tree.isLeaf()) {
+//            // apply repulsive force
+//            int forceMultiplier = tree.getTotalContainedItemCount();
+//            for (ForceConnectedNode node : leaf.getContainedItems()) {
+//                double dx = getDx(node, tree);
+//                double dy = getDy(node, tree);
+//                applyRepulsionForce(node, dx, dy, forceMultiplier, forceMagnitude);
+//            }
+//        } else {
+//            for (QuadTree<ForceConnectedNode> treeNode : tree.getSubTrees()) {
+//                applyTreeForce(leaf, treeNode);
+//            }
+//        }
     }
 
     private static double getDx(ForceConnectedNode node, QuadTree tree) {
@@ -102,11 +101,13 @@ public class NodeForceCalculator {
         return node.getX() - treeCenterOfGravity.y;
     }
 
-    private static double quadTreeDistance(QuadTree leaf, QuadTree tree) {
-        Bounds leafBounds = leaf.getBounds();
+    private static double quadTreeDistance(ForceConnectedNode node, QuadTree tree) {
         Coordinate treeCenterOfGravity = tree.getCenterOfGravity();
-        double dx = leafBounds.centerX() - treeCenterOfGravity.x;
-        double dy = leafBounds.centerY() - treeCenterOfGravity.y;
+        if (treeCenterOfGravity == null) {
+            return 0;
+        }
+        double dx = node.getX() - treeCenterOfGravity.x;
+        double dy = node.getY() - treeCenterOfGravity.y;
         return Math.sqrt(dx * dx + dy * dy);
     }
 
@@ -117,17 +118,21 @@ public class NodeForceCalculator {
     void attractNodes(ForceConnectedNode nodeA, ForceConnectedNode nodeB) {
         double dx = nodeB.getX() - nodeA.getX();
         double dy = nodeB.getY() - nodeA.getY();
-        double distance = Math.sqrt(dx * dx + dy * dy);
-//        double dx = nodeA.getX() - nodeB.getX();
-//        dx *= dx;
-//
-//        double dy = nodeA.getY() - nodeB.getY();
-//        dy *= dy;
-//
-//        double fx = Math.min(maxForce, dx / optimalDistance);
-//        double fy = Math.min(maxForce, dy / optimalDistance);
-//
-//        nodeA.applyForce(fx, fy);
-//        nodeB.applyForce(-fx, -fy);
+
+        double mag = Math.sqrt(dx * dx + dy * dy);
+        if (mag == 0) {
+            return;
+        }
+        double vx = dx / mag;
+        double vy = dy / mag;
+
+        double force = Math.min(maxForce, mag * mag / optimalDistance); // Math.log(mag) / optimalDistance;
+
+        double fx = vx * force;
+        double fy = vy * force;
+
+        nodeA.addForce(-fx, -fy);
+        nodeB.addForce(fx, fy);
+        Timber.i("attractNodes " + nodeA.getIndex() + " " + nodeB.getIndex() + " magnitude: " + force + "   dx,dy: " + dx + ", " + dy + "   vx,vy: " + vx + ", " + vy + "   fx,fy: " + fx + ", " + fy);
     }
 }
